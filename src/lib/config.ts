bd433654dd8e0f8d8f3aacb1809f14f4893b357b
Alejandro Quintana <alejandro_hernandez_00@hotmail.com>
2025-06-25 01:02:00 -0300

    Fixed URL Path for environments

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,5 +3,5 @@ declare const __BRANCH__: string;
 
 export const API_BASE_URL =
   __BRANCH__ === "main"
-    ? "https://recipes-backend.alejandro-hernandez-00.workers.dev"
-    : "https://recipes-backend-dev.alejandro-hernandez-00.workers.dev";
+    ? "https://recipes-backend-production.alejandro-hernandez-00.workers.dev"
+    : "https://recipes-backend.alejandro-hernandez-00.workers.dev";
