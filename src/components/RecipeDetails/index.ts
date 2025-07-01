d20a9ef024611e95733180be43ac0ebf05efc31e
Alejandro Quintana <alejandro_hernandez_00@hotmail.com>
2025-07-01 09:53:35 -0300

    fix(build): exporta RecipeMetaPanel desde index.ts para evitar error de compilación
    - Añade exportación faltante del nuevo componente
    - Permite que RecipeDetails.tsx lo importe correctamente
    - Corrige error de Rollup durante el build

--- src/components/RecipeDetails/index.ts
+++ src/components/RecipeDetails/index.ts
@@ -3,4 +3,5 @@ export { default as RecipeHeaderEditor } from "./RecipeHeaderEditor";
 export { default as ComponentList } from "./ComponentList";
 export { default as AddComponentForm } from "./AddComponentForm";
 export { default as CostCalculator } from "./CostCalculator";
+export { default as RecipeMetaPanel } from "./RecipeMetaPanel";
 export * from "./types";
